53357953205b7a3d6ebe13e73e74d43ea82acce2
Mahathi Amencherla <mahathiamencherla15@gmail.com>
2020-05-08 19:53:33 +0530

    Auth token comment

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,9 @@ const Planner = require('../models/planner')
 
 const auth = async (req, res, next) => {
     try {
-        const token = req.params.token
+        // auth flow assumes that the token is given as a url parameter
+        // example url: http://localhost:3001/me/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWI1NjY5MjBhNDNkOTZiN2FhYjhkN2IiLCJpYXQiOjE1ODg5NDcwNDF9.I2-g2q_bK6ptT9l1GFFFqFU2NmMBdnI8oSOpsevdfHU
+        const token = req.params.token 
         const decoded = jwt.verify(token, 'thisistravelloplanner')
         const planner = await Planner.findOne({ _id: decoded._id, 'tokens.token': token })
 
